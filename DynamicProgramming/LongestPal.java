1cbf6bdc1874bb39d811b898bb797dea47900245
Meghana <meghanakongara0429@gmail.com>
2025-07-10 10:16:30 +0530

    Longest Palindrome substring using DP

--- DynamicProgramming/LongestPal.java
+++ DynamicProgramming/LongestPal.java
@@ -1,14 +1,14 @@
 import java.util.*;
 class LongestPal
 {
-    public static int isPal(String s)
+    public static String isPal(String s)
     {
         int n=s.length();
         int dp[][]=new int[n][n];
         for(int i=0;i<n;i++)
         dp[i][i]=1;
         int maxLen=1;
-        int st=1;
+        int st=0;
         for(int i=0;i<n-1;i++)
         {
             if(s.charAt(i)==s.charAt(i+1))
@@ -39,7 +39,7 @@ class LongestPal
                 }
             }
         }
-        return maxLen;
+        return s.substring(st,st+maxLen);
     }
     public static void main(String args[])
     {
